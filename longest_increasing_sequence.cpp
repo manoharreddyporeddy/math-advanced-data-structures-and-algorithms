3e58fb95d538cd41c4d47d43ab35c7d52986914a
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2017-10-17 08:57:32 +0530

    Update longest_increasing_sequence.cpp

--- longest_increasing_sequence.cpp
+++ longest_increasing_sequence.cpp
@@ -1,10 +1,19 @@
 /*
 
 Let L be the list, that contains N elements, say, from 0 to N-1
+Make one pass from left to right of list L, from 0th index element to N-1 index element
 
-1. Make one pass from left to right of list, from 0th element to N-1 element
-2. While the pass is made
-3.    If it is increasing, continue the pass
-4. 
-
+Save the 1st element (0th index) in List S, continue the pass with 2nd element (1st index)
+While the pass is still on:
+    If next is increasing of current
+           SAVE into S, and then continue the pass, with next element in list (NOTE: S list has increasing elements)(#)
+           Also, SAVE, from which element this next element came from (possibly index) (##)
+    else:
+           Find the next's position in S, using binary search (see, NOTE above, S list is ascending order)
+           If S is found:
+                   nothing to do
+           else:
+                   it is possible that next is not found, but you will find the element that is > next, say it nextOfnext
+                   REPLACE nextOfnext with next, in S list (REPLACE is similar to SAVE above, #)
+                   Also, SAVE, from which element this next element came from (possibly index) - similar to (##) above - this will be same as from which element nextOfnext came from
 */
