cf6bde362281c41f94b9cbbc1f66d29b6f221bc4
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2017-10-17 08:46:28 +0530

    Update longest_increasing_sequence.cpp

--- longest_increasing_sequence.cpp
+++ longest_increasing_sequence.cpp
@@ -2,6 +2,9 @@
 
 Let L be the list, that contains N elements, say, from 0 to N-1
 
-
+1. Make one pass from left to right of list, from 0th element to N-1 element
+2. While the pass is made
+3.    If it is increasing, continue the pass
+4. 
 
 */
