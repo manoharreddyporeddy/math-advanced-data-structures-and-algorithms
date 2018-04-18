ca6cd7ef5ceb08d45e9ec8e0cbc4fec922633a60
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2018-04-18 20:26:36 +0530

    Update PRINT-EVERYTHING.cpp

--- PRINT-EVERYTHING.cpp
+++ PRINT-EVERYTHING.cpp
@@ -1,1 +1,11 @@
 
+
+	template <class T>
+	static void printA(T *A, size_t N, bool bNewLine = true) {	// print A[]
+		for (size_t i = 0; i < N; i++) {
+			cout << A[i] << " ";
+		}
+		if (bNewLine) {
+			cout << "\n";
+		}
+	}
