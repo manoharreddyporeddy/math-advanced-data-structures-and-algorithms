d33ce4fe849abefccb6e8fff387518f24d34d091
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2020-07-26 11:46:06 +0530

    more comments

--- js-backtracking-knight-movement.js
+++ js-backtracking-knight-movement.js
@@ -1,4 +1,22 @@
-// Java program for Knight Tour problem
+// Knight Tour problem
+
+// GOAL:
+//      knight on the top-left corner (0)
+//          should jump to
+//              all squares
+//              on chess board / (8x8) matrix
+
+/*
+    0	59	38	33	30	17	8	63
+    37	34	31	60	9	62	29	16
+    58	1	36	39	32	27	18	7
+    35	48	41	26	61	10	15	28
+    42	57	2	49	40	23	6	19
+    47	50	45	54	25	20	11	14
+    56	43	52	3	22	13	24	5
+    51	46	55	44	53	4	21	12
+*/
+
 class KnightTour {
     //
 
@@ -33,11 +51,10 @@ class KnightTour {
         );
     }
 
-    // Solve Knight Tour problem
     solveKTUtil(x, y, movei) {
         if (movei == this.N * this.N) return true;
 
-        // Try all next moves from the current x, y
+        // try all next moves from the current x, y
         for (let k = 0; k < this.N; k++) {
             //
 
@@ -86,7 +103,7 @@ class KnightTour {
             }
         }
 
-        // Since the Knight is initially at the first block
+        // Knight is initially at the first block
         this.aa[0][0] = 0;
 
         /*
@@ -100,7 +117,7 @@ class KnightTour {
             -1	-1	-1	-1	-1	-1	-1	-1
         */
 
-        // Start from 0,0 and explore all tours using solveKTUtil()
+        // start from 0,0 and explore all tours
         if (this.solveKTUtil(0, 0, 1)) {
             this.printSolution();
         } else {
