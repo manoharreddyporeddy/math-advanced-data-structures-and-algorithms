3a5498f6a07e904af68e55df70fd422534f017c1
Manohar Reddy Poreddy <manoharporeddy@outlook.com>
2021-04-18 13:29:23 +0530

    Update ---code-template-codechef.js

--- ---code-template-codechef.js
+++ ---code-template-codechef.js
@@ -1,34 +1,23 @@
-// NODEJS common template for everyone - submit answer here https://www.codechef.com/submit/FLOW001 - problem description https://www.codechef.com/problems/FLOW001
-
-var tempStr = "";
-
-process.stdin.resume();
-process.stdin.setEncoding("utf8");
-process.stdin.on("data", (chunk) => (tempStr += chunk));
-process.stdin.on("end", () => main(tempStr.split("\n")));
-
-const main = (INPUT_LINE_ARRAY) => {
-    // 1st line - number of tests (T)
-    var T = +INPUT_LINE_ARRAY[0];
-
-    // remaining lines   are   input tests
-    for (let i = 1; i <= T; i++) {
-        // each line has 2 numbers separated by space  e.g;    x y
-        let [a, b] = INPUT_LINE_ARRAY[i].split(" ").map((x) => +x);
-
-        console.log(a + b);
-    }
-};
-
-
-// ------------------------------------------------
-
-// NODEJS common template for everyone - problem HS08TEST
-var inputStr = "";
+let inputStr = "";
 const _toInt = (x) => +x;
 process.stdin.resume();
 process.stdin.setEncoding("utf8");
 process.stdin.on("data", (chunk) => (inputStr += chunk));
+
+// NODEJS common template for everyone - problem FLOW001
+process.stdin.on("end", () => {
+    const INPUT_LINES__ARR = inputStr.split("\n");
+
+    var T = +INPUT_LINES__ARR[0]; // # of tests
+
+    for (let i = 1; i <= T; i++) {
+        let [a, b] = INPUT_LINES__ARR[i].split(" ").map(_toInt); // each test
+
+        console.log(a + b);
+    }
+});
+
+// NODEJS common template for everyone - problem HS08TEST
 process.stdin.on("end", () => {
     let [a, b] = inputStr.split(" ").map(_toInt);
 
@@ -41,5 +30,3 @@ process.stdin.on("end", () => {
 
     console.log(res);
 });
-
-// ------------------------------------------------
